dffab4180766422c898ec95ad75b9b646f059951
Broberg, Gage <gage.broberg@fmr.com>
2025-01-06 20:31:34 -0700

    Adding initial chart

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,23 @@
 import React from "react";
-import { Button, SafeAreaView, View, Text } from "react-native";
-import { TextInput } from "react-native";
+import { SafeAreaView, View, Text } from "react-native";
 import DateTimePicker, { DateTimePickerEvent } from '@react-native-community/datetimepicker';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
+const data = [
+  {
+    name: 'Current Age',
+    withdrawalNow: 0,
+  },
+  {
+    name: 'FRA',
+    withdrawalNow: 3000,
+    withdrawalAtFRA: 0
+  },
+  {
+    name: '90',
+    withdrawalNow: 6000,
+    withdrawalAtFRA: 10000
+  },
+];
 
 export default function Index() {
   const [date, setDate] = React.useState(new Date(1598051730000));
@@ -18,20 +34,31 @@ export default function Index() {
       justifyContent: "center",
       alignItems: "center",
     }}>
-    <SafeAreaView
-    style={{
-      flexDirection: "row",
-      justifyContent: "center",
-      alignItems: "center",
-    }}>
-      <Text style={{marginRight: 10}}>Hello</Text>
-      <DateTimePicker
-      testID="dateTimePicker"
-      value={date}
-      mode='date'
-      onChange={onChange}
-      />
-    </SafeAreaView>
+      <SafeAreaView
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+        }}>
+          <Text style={{marginRight: 10}}>Hello</Text>
+          <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode='date'
+          onChange={onChange}
+          />
+      </SafeAreaView>
+    
+      <Text>Early Withdrawal Breakdown</Text>
+      <LineChart width={400} height={300} data={data} margin={{ top: 5, right: 20, left: 10, bottom: 5 }}>
+        <Line type="monotone" dataKey="withdrawalNow" stroke="#8884d8" />
+        <Line type="monotone" dataKey="withdrawalAtFRA" stroke="#82ca9d" />
+        <CartesianGrid stroke="#ccc"/>
+        <XAxis dataKey="name"/>
+        <YAxis/>
+        <Tooltip/>
+        <Legend/>
+      </LineChart>
     </View>
   );
 }
